dbc899d78aa6c8da0025b9c4496c3fbaf75b0b3e
mulveys7619 <mulveyseanm@gmail.com>
2025-03-24 20:36:05 -0400

    Separate cmd from arg and with cmd is echo, print arg

--- src/main.rs
+++ src/main.rs
@@ -15,11 +15,17 @@ fn main() {
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
 
-
+        
+        // Isolate the command ny separating at white space
+       let command = get_command(input.clone());
+       let arg = get_arg(input.clone());
         // Switch for commands
-        match input.trim() {
+        match command.trim() {
+            "echo" => {
+                echo(arg);
+            },
             // Exit command
-            "exit 0" => break,
+            "exit" => break,
             _ =>
             { // Invalid command
             println!("{}: command not found", input.trim())
@@ -31,4 +37,48 @@ fn main() {
 
     }
     
+}
+
+fn echo(s: String) -> String {
+    println!("{s}");
+    s
+}
+
+fn get_command(s: String) -> String{
+    let command = {
+    let mut c = String::from("");
+    for i in s.chars() {
+        if i != ' '{
+            c.push(i);
+        }else{
+            break;
+        }
+    }
+    c
+    };
+    command
+
+}
+
+fn get_arg(s: String) -> String{
+    let mut cOver = false;
+
+    let arg =
+    {
+        let mut a = String::from("");
+        for i in s.chars(){
+            if cOver {
+                a.push(i);
+            
+            }
+        if i == ' '{
+            cOver = true;
+        }
+        
+        }
+        a
+    };
+    arg
+
+
 }
